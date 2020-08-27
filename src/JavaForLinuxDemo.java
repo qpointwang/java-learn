0e905ffd47077832782f1cdb290675e5ece3ef3b
wchee3 <qpointwang@163.com>
2020-08-27 17:09:13 +0800

    Linux command

--- src/JavaForLinuxDemo.java
+++ src/JavaForLinuxDemo.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * @anthor qpointwang
@@ -8,7 +11,18 @@ public class JavaForLinuxDemo {
 
     public static void main(String[] args) {
         try {
-            Process process = Runtime.getRuntime().exec("ls ./");
+            Process process = Runtime.getRuntime().exec("ifconfig");
+            //取得命令结果的输出流
+            InputStream inputStream = process.getInputStream();
+            //用一个读输出流类去读
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            //用缓冲器读行
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String line = null;
+            //直到读完为止
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
