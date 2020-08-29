1883a285823d4d6969ac9c3ee3225383294b388b
wchee3 <qpointwang@163.com>
2020-08-29 16:39:56 +0800

    thread.sleep和object.wait的区别

--- src/WaitAndNotifyDemo.java
+++ src/WaitAndNotifyDemo.java
@@ -20,7 +20,10 @@ public class WaitAndNotifyDemo {
             // 在线线程中调用wait方法的时候 要用synchronized锁住对象，确保代码段不会被多个线程调用
             synchronized (person) {
                 System.out.println(Thread.currentThread().getName()+" "+"is waiting");
+                // Thread.sleep 不会让出对象锁，在sleep期间不允许其他线程访问person代码块
+                Thread.sleep(2000);
                 // 调用某个对象的wait()方法能让当前线程阻塞，并且当前线程必须拥有此对象的monitor
+                // wait()会让出对象锁，wait后其他线程可以访问person代码块
                 person.wait(5000);
                 System.out.println(Thread.currentThread().getName()+" "+"is notified");
             }
@@ -32,7 +35,9 @@ public class WaitAndNotifyDemo {
     public void test2() {
         synchronized (person) {
             // 唤醒一个当前对象上的等待线程
+            System.out.println("开始唤醒");
             person.notify();
+            System.out.println("成功唤醒");
             // 唤醒所有当前对象上的等待线程
             // person.notifyAll();
         }
